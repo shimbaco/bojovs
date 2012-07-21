867a5ededbc5d128622421abd6fac2f9e248ecfa
Koji Shinba <bojovs@gmail.com>
2012-07-21 14:21:24 +0900

    notes -> note

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Bojovs::Application.routes.draw do
     root to: 'dashboard#index'
   end
 
-  match ':year/:month/:day/:slug' => 'notes#show', as: :notes,
+  match ':year/:month/:day/:slug' => 'notes#show', as: :note,
     year: /[0-9]{4}/, month: /[0-9]{2}/, day: /[0-9]{2}/, slug: /([a-z]|[A-Z]|[0-9]|-)+/
 
   root to: 'home#index'
