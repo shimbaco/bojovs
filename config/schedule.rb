ed63aa8ad07f6f209a26f8e35470484fbe4a247b
Koji Shinba <bojovs@gmail.com>
2012-09-19 01:23:09 +0900

    backupディレクトリをconfigディレクトリ以下に移動

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
 # every 1.day, at: '5:00 am' do
 every 5.minutes do
-  command 'RAILS_ENV=production bundle exec backup perform -t database -c "backup/config.rb"'
+  command 'RAILS_ENV=production bundle exec backup perform -t database -c "config/backup/config.rb"'
 end
