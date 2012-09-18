d162ce55caac93abe1e8a81d9a0f30d011f1991e
Koji Shinba <bojovs@gmail.com>
2012-09-19 01:36:20 +0900

    ログを出力するよう修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,6 @@
+set :output, '/home/com-bojovs/shared/log/whenever.log'
+
 # every 1.day, at: '5:00 am' do
 every 5.minutes do
-  command 'RAILS_ENV=production bundle exec backup perform -t database -c "config/backup/config.rb"'
+  command 'RAILS_ENV=:environment bundle exec backup perform -t database -c "config/backup/config.rb"'
 end
