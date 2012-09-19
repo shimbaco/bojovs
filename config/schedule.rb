8935441442461b82612a17943e432756830dd164
Koji Shinba <bojovs@gmail.com>
2012-09-19 12:16:15 +0900

    current_pathが呼び出せなかったのでハードコーディング

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ set :output, '/home/com-bojovs/shared/log/whenever.log'
 
 # every 1.day, at: '5:00 am' do
 every 5.minutes do
-  command "cd #{current_path}; RAILS_ENV=:environment bundle exec backup perform --trigger database --config-file 'config/backup/config.rb'"
-  command "cd #{current_path}; RAILS_ENV=:environment bundle exec backup perform --trigger log --config-file 'config/backup/config.rb'"
+  command "cd /home/com-bojovs/current; RAILS_ENV=:environment bundle exec backup perform --trigger database --config-file 'config/backup/config.rb'"
+  command "cd /home/com-bojovs/current; RAILS_ENV=:environment bundle exec backup perform --trigger log --config-file 'config/backup/config.rb'"
 end
