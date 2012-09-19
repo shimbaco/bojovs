c1ed3198a0e8aff7082ad41760681d67c6af75f6
Koji Shinba <bojovs@gmail.com>
2012-09-19 15:10:37 +0900

    式展開するよう修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,5 +3,5 @@ set :output, '/home/com-bojovs/shared/log/whenever.log'
 # every 1.day, at: '5:00 am' do
 every 5.minutes do
   command "cd :current_path; RAILS_ENV=:environment bundle exec backup perform --trigger database --config-file 'config/backup/config.rb'"
-  command "cd :current_path; RAILS_ENV=:environment bundle exec backup perform --trigger log --config-file 'config/backup/config.rb'"
+  command "cd #{current_path}; RAILS_ENV=#{environment} bundle exec backup perform --trigger log --config-file 'config/backup/config.rb'"
 end
