e0445f203cedf4551fddcb1eb4b6870ca8a715ea
Koji Shinba <bojovs@gmail.com>
2012-09-23 03:20:39 +0900

    管理画面 ノート 一覧 日付のフォーマットを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def formated_date(date)
+    date.strftime('%Y/%m/%d')
+  end
+
+  def formated_datetime(datetime)
+    datetime.strftime('%Y/%m/%d %H:%M:%S')
+  end
 end
