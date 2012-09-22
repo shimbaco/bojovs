35c94144ed91c3aa676a3da7287b1287cbed8922
Koji Shinba <bojovs@gmail.com>
2012-09-23 01:12:52 +0900

    タイムゾーンを東京に変更

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,12 @@ module Bojovs
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Tokyo'
+
+    # config.active_record.default_timezone determines whether to use Time.local (if set to :local) or
+    # Time.utc (if set to :utc) when pulling dates and times from the database.
+    # The default is :utc for Rails, although Active Record defaults to :local when used outside of Rails.
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
