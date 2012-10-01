c3c0344b9a97f8810f63c1ccd1f6e7229a8ff69f
Koji Shinba <bojovs@gmail.com>
2012-10-02 00:52:43 +0900

    ノート 一覧ページの実装

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,10 @@
 # coding: utf-8
 
 class NotesController < ApplicationController
+  def index
+    @notes = Note.order('published_at DESC')
+  end
+
   def show
     date = Date.parse(params[:year] + params[:month] + params[:day])
     @note = Note.where(published_at: date).where(slug: params[:slug]).first!
--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -3,6 +3,20 @@
 require 'spec_helper'
 
 describe 'ノート' do
+  describe '一覧' do
+    before do
+      FactoryGirl.create(:note, published_at: '2012/06/01')
+      FactoryGirl.create(:note, published_at: '2012/07/01')
+      FactoryGirl.create(:note, published_at: '2012/08/01')
+    end
+
+    it '一番上に最新の記事が表示される' do
+      visit '/notes'
+
+      page.first('ul.notes li').should have_content('Hello World 3')
+    end
+  end
+
   describe '詳細' do
     before do
       FactoryGirl.create(:note, published_at: '2012/06/01')
